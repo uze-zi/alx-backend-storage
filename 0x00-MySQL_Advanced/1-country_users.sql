bdf837871139180a370ac2ab60bf1a6fabc9570c
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-10-21 03:59:42 +0100

    added solution for #1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -8,6 +8,8 @@ CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255),
-	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 	PRIMARY KEY (id)
-)
+);
+
+-- Alter the table to add a new colunn
+ALTER TABLE users ADD COLUMN country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US';
