f7975c7ad57a4c6634d729edb8dae48fe1a03f65
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-10-26 18:25:12 +0100

    modified # 2 to meet requirements

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,10 @@
--- Calculate number of fans per band and rank them
+/*
+Calculate number of fans
+per band and rank them
+*/
 
 SELECT origin, COUNT(*) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fans DESC
+ORDER BY nb_fans DESC;
+
