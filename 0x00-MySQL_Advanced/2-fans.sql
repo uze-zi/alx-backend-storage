9afe7e8845a75e5f3630c4b44143cc1bee0a4f34
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-10-26 18:31:32 +0100

    updated 2-fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,10 +1,10 @@
 /*
-Calculate number of fans
+Calculate number of (non-unique)fans
 per band and rank them
 */
 
-SELECT origin, COUNT(*) AS nb_fans
-FROM metal_bands
+
+SELECT origin, COUNT(*) AS nb_fans FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
 
